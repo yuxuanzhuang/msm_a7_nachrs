ac00cfdc3567f6d1ac15b5845a0cd8a3349a5f6f
yuxuan.zhuang@dbb.su.se <yuxuan.zhuang@dbb.su.se>
2022-10-10 10:42:43 +0200

    add new network lindahl

--- msm_a7_nachrs/dask_worker/dask_cluster_tcblab.py
+++ msm_a7_nachrs/dask_worker/dask_cluster_tcblab.py
@@ -37,6 +37,12 @@ try:
     ip_address = ni.ifaddresses('ens6')[ni.AF_INET][0]['addr']
 except BaseException:
     pass
+try:
+    ni.ifaddresses('ens8')
+    ip_address = ni.ifaddresses('ens8')[ni.AF_INET][0]['addr']
+except BaseException:
+    pass
+
 
 port = 8786
 
@@ -53,7 +59,7 @@ class TcllabSLURMJob(SLURMJob):
         self.jobscript.append("#SBATCH --cpus-per-task=1\n")
         self.jobscript.append("#SBATCH --ntasks-per-core=1\n")
         self.jobscript.append("#SBATCH --ntasks-per-node=8\n")
-        self.jobscript.append("#SBATCH -t 24:00:00\n")
+        self.jobscript.append("#SBATCH -t 48:00:00\n")
         self.jobscript.append("#SBATCH -C cpu\n")
         self.jobscript.append("\n")
         self.jobscript.append(
@@ -70,6 +76,98 @@ class TcllabSLURMJob(SLURMJob):
         proc = subprocess.Popen(
             ['sbatch', 'submit.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             cwd='/nethome/yzhuang/dask-worker-space'
+        )
+
+        out, err = proc.communicate()
+        out, err = out.decode(), err.decode()
+
+        if proc.returncode != 0:
+            raise RuntimeError(
+                "Command exited with non-zero exit code.\n"
+                "Exit code: {}\n"
+                "stdout:\n{}\n"
+                "stderr:\n{}\n".format(proc.returncode, out, err)
+            )
+
+        self.jobid = out.split(' ')[-1][:-1]
+
+class LindahlSLURMJob(SLURMJob):
+    def _generate_job(self):
+        self.jobscript = []
+        self.jobscript.append("#!/usr/bin/env bash \n")
+        self.jobscript.append("#SBATCH -J dask-worker\n")
+#        self.jobscript.append("#SBATCH -p lindahl\n")
+        self.jobscript.append("#SBATCH -p lindahl\n")
+
+        self.jobscript.append("#SBATCH --nodes=" + str(self.n_nodes) + '\n')
+        self.jobscript.append("#SBATCH --cpus-per-task=1\n")
+        self.jobscript.append("#SBATCH --ntasks-per-core=1\n")
+        self.jobscript.append("#SBATCH --ntasks-per-node=24\n")
+        self.jobscript.append("#SBATCH -t 48:00:00\n")
+        self.jobscript.append("#SBATCH -C gpu\n")
+        self.jobscript.append("\n")
+        self.jobscript.append(
+            "srun " +
+            dask_worker_dir +
+            self.ip_address +
+            ":" +
+            self.port +
+            " --memory-limit=auto --nthreads 2 --nprocs 1 --death-timeout 60\n")
+
+    def submit_job(self):
+        with open('/nethome/yzhuang/dask-worker-space-lindahl/submit.sh', "w") as f:
+            f.writelines(self.jobscript)
+        proc = subprocess.Popen(
+            ['sbatch', 'submit.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cwd='/nethome/yzhuang/dask-worker-space-lindahl'
+        )
+
+        out, err = proc.communicate()
+        out, err = out.decode(), err.decode()
+
+        if proc.returncode != 0:
+            raise RuntimeError(
+                "Command exited with non-zero exit code.\n"
+                "Exit code: {}\n"
+                "stdout:\n{}\n"
+                "stderr:\n{}\n".format(proc.returncode, out, err)
+            )
+
+        self.jobid = out.split(' ')[-1][:-1]
+
+
+class NewLindahlSLURMJob(SLURMJob):
+    def _generate_job(self):
+        self.jobscript = []
+        self.jobscript.append("#!/usr/bin/env bash \n")
+        self.jobscript.append("#SBATCH -J dask-worker\n")
+#        self.jobscript.append("#SBATCH -p lindahl\n")
+        self.jobscript.append("#SBATCH -p lindahl1,lindahl2,lindahl3,lindahl4\n")
+
+        self.jobscript.append("#SBATCH --nodes=" + str(self.n_nodes) + '\n')
+        self.jobscript.append("#SBATCH --cpus-per-task=1\n")
+        self.jobscript.append("#SBATCH --ntasks-per-core=1\n")
+        self.jobscript.append("#SBATCH --ntasks-per-node=24\n")
+        self.jobscript.append("#SBATCH -t 48:00:00\n")
+        self.jobscript.append("#SBATCH --gres=gpu:4\n")
+        self.jobscript.append("\n")
+        self.jobscript.append(
+            "srun " +
+            dask_worker_dir +
+            self.ip_address +
+            ":" +
+            self.port +
+            " --memory-limit=auto --nthreads 2 --nprocs 1 --death-timeout 60\n")
+
+    def submit_job(self):
+        with open('/nethome/yzhuang/dask-worker-space-lindahl/submit.sh', "w") as f:
+            f.writelines(self.jobscript)
+            
+        proc = subprocess.Popen(
+            [
+            '/opt/tcbsys/slurm/22.05.3/bin/sbatch',
+            'submit.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cwd='/nethome/yzhuang/dask-worker-space-lindahl'
         )
 
         out, err = proc.communicate()
